234bb1773ac4df0f5563a29ec2b061ecc9683c47
Michael Riedmann <michael.riedmann@cloudflight.io>
2022-01-25 04:15:59 +0100

    build script cleanup

--- build.ps1
+++ build.ps1
@@ -27,36 +27,28 @@ if((git diff --stat)){
     throw "Working dir is dirty!"
 }
 
-Invoke-Git checkout main
+Invoke-Git checkout gh-pages
 Invoke-Git checkout -b "update-$version"
+
+Invoke-Git checkout main *.ps1 *.py *.sql *.libsonnet *.pfx
+Invoke-Git checkout main packages/ source/
+Invoke-Git restore --staged *
 
 Remove-Item -Recurse -Force -Path .\manifests -ErrorAction SilentlyContinue
 Remove-Item -Force -Path .\source.msix -ErrorAction SilentlyContinue
 
 ((Get-Content -path .\source\AppxManifest.xml -Raw) -replace ' Version="[0-9\.]+" />'," Version=`"$version`" />") | Set-Content -Encoding Default -Path .\source\AppxManifest.xml
 
-.\generate.ps1
+.\01-generate-manifests.ps1
 Check-LastCommand "generate failed"
 
-py .\build-index.py
+py .\02-build-index-db.py
 Check-LastCommand "index-db build failed"
 
-.\Build-Index-Package.ps1
-.\Sign-Index-Package.ps1
+.\03-build-index-package.ps1
+.\04-sign-index-package.ps1
 
 Invoke-Git add source.msix
 Invoke-Git add manifests/
 Invoke-Git commit -m "new index published"
 Invoke-Git reset --hard
-Invoke-Git checkout gh-pages
-
-Remove-Item -Recurse -Force -Path .\manifests -ErrorAction SilentlyContinue
-Remove-Item -Force -Path .\source.msix -ErrorAction SilentlyContinue
-
-Invoke-Git checkout "update-$version" manifests\
-Invoke-Git checkout "update-$version" source.msix
-Invoke-Git add source.msix
-Invoke-Git add manifests/
-Invoke-Git commit -m "new index published"
-
-Invoke-Git branch -D "update-$version"
