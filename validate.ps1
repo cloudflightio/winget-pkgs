e2666d371a6b4ed6ee68cc6e62c6d0eac6d0712e
Michael Riedmann <michael.riedmann@cloudflight.io>
2022-01-25 03:47:41 +0100

    fixed validate script

--- validate.ps1
+++ validate.ps1
@@ -1,6 +1,9 @@
 Get-ChildItem ".\manifests" -Recurse -Directory | % {
     $dotCount = ($_.BaseName.ToCharArray() | Where-Object {$_ -eq '.'} | Measure-Object).Count
     if ($dotCount -ge 2) {
+        Get-ChildItem $_.FullName -Filter "*.yaml" | Where-Object { $_.BaseName -match "[0-9a-z]{4}-" } | % {
+            Remove-Item $_.FullName
+        }
         Write-Host $_.FullName
         winget validate --manifest $_.FullName
     }
