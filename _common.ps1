a77447f9a68f547e7ccc6cbfd283824b55a63105
Michael Riedmann <michael.riedmann@cloudflight.io>
2022-01-26 15:39:19 +0100

    extracted common functions to ease debugging

---
+++ _common.ps1
@@ -0,0 +1,101 @@
+function Invoke-GithubApi {
+    param(
+        [Parameter(
+            ValueFromRemainingArguments=$true
+        )][string[]]
+        $listArgs
+    )
+    $headers = $null
+    if ($env:GITHUB_TOKEN) {
+        $headers = @{ Authorization = "Bearer "+ $env:GITHUB_TOKEN }
+    } elseif ($env:GITHUB_USER -and $env:GITHUB_PASS) {
+        $headers = @{ Authorization = "Basic "+ [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$($env:GITHUB_USER):$($env:GITHUB_PASS)")) }
+    }
+    Invoke-WebRequest @listArgs -Headers $headers
+}
+
+function Get-GithubReleases {
+    param (
+        [Parameter(Position = 0, Mandatory = $true)]$Repo
+    )
+    
+    $releases = "https://api.github.com/repos/$repo/releases"
+    return (Invoke-GithubApi $releases | ConvertFrom-Json)
+}
+
+function Get-GithubAssets {
+    param (
+        [Parameter(Position = 0, Mandatory = $true)]$Repo,
+        [Parameter(Position = 1, Mandatory = $true)]$ReleaseId
+    )
+
+    $assets = "https://api.github.com/repos/$Repo/releases/$ReleaseId/assets"
+    return (Invoke-GithubApi $assets | ConvertFrom-Json)
+}
+
+function Get-Property ($Object, $PropertyName, [object[]]$ArgumentList) {
+    return $Object.GetType().InvokeMember($PropertyName, 'Public, Instance, GetProperty', $null, $Object, $ArgumentList)
+}
+
+function Get-MsiInfo {
+    param (
+        [Parameter(Position = 0, Mandatory = $true)]$Url
+    )
+
+    $windowsInstaller = New-Object -ComObject WindowsInstaller.Installer
+
+    $urlHashStream = [IO.MemoryStream]::new([byte[]][char[]]$Url)
+    $urlHash = (Get-FileHash -InputStream $urlHashStream -Algorithm MD5).Hash
+
+    $FileName = $urlHash + "-" + $Url.Split('/')[-1]
+    $FilePath = ".tmp\$FileName"
+    if(-not (Test-Path -Path $FilePath)) {
+        Start-BitsTransfer -Source $Url -Destination $FilePath
+    }
+
+    $MSI = $windowsInstaller.OpenDatabase($FilePath, 0)
+
+    $View = $MSI.OpenView("SELECT * FROM Property")
+    $View.Execute()
+
+    $hash = @{}
+    while ($Record = $View.Fetch()) {
+        $name = Get-Property $Record StringData 1
+        $value = Get-Property $Record StringData 2
+        $hash.Add($name,$value)
+    }
+    $View.Close()
+    
+    $checksum = (Get-FileHash -Path $FilePath -Algorithm SHA256).Hash
+
+    return  [pscustomobject]@{
+        Version = $hash.ProductVersion
+        ProductCode = $hash.ProductCode
+        UpgradeCode = $hash.UpgradeCode
+        Hashsum =     $checksum
+    }
+}
+
+function Get-ProductReleases {
+    param (
+        [Parameter(Position = 0, Mandatory = $true)]$Repo
+    )
+
+    @(Get-GithubReleases -Repo $Repo | ForEach-Object {
+        [pscustomobject]@{
+            Release = $_
+            Assets = (Get-GithubAssets -Repo $Repo -ReleaseId $_.id | 
+                Where-Object { $_.name -like "*.msi" } |
+                ForEach-Object {
+                    $msi = (Get-MsiInfo -Url $_.browser_download_url)
+                    [pscustomobject]@{
+                        PackageVersionObject = [System.Version]$msi.Version
+                        PackageVersion = $msi.Version
+                        InstallerUrl = $_.browser_download_url
+                        InstallerSha256 = $msi.Hashsum
+                        ProductCode = $msi.ProductCode
+                    }
+                })
+        }
+    })
+}
