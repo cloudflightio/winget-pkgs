d54ef3d9ffc49cb8a21e56ad2f054dfc695d2a7e
Michael Riedmann <michael_riedmann@live.com>
2023-01-24 05:04:37 +0100

    using dotnet tool restore on azuresigntool

--- 04-sign-index-package-release.ps1
+++ 04-sign-index-package-release.ps1
@@ -1,17 +1,20 @@
 $DescriptionURL = "https://github.com/cloudflightio/dockerinwsl"
-$AzureKeyVaultURL = "https://cloudflight-code-signing.vault.azure.net"
 $AzureKeyVaultTenantId = "e21ebe2c-3b5b-4d4c-8d0e-c1ca0e8ea14b"
-$AzureKeyVaultClientId = "18462f44-aee3-42ac-aba8-bdfd3d4d8c23"
-$AzureKeyVaultCertificate = "globalsign-ev-code-signing"
 $TimestampService = "http://timestamp.digicert.com"
 
-& AzureSignTool sign `
+$AzureKeyVaultURL = "https://cloudflight-code-signing.vault.azure.net"
+$AzureKeyVaultClientId = "18462f44-aee3-42ac-aba8-bdfd3d4d8c23"
+$AzureKeyVaultClientSecret = "$env:AZURE_SIGN_CLIENT_SECRET"
+$AzureKeyVaultCertificate = "globalsign-ev-code-signing"
+
+& dotnet tool restore
+& dotnet tool run azuresigntool sign `
     -du "$DescriptionURL" `
     -fd sha256 `
     -kvu "$AzureKeyVaultURL" `
     -kvi "$AzureKeyVaultClientId" `
     -kvt "$AzureKeyVaultTenantId" `
-    -kvs "$env:AZURE_SIGN_CLIENT_SECRET" `
+    -kvs "$AzureKeyVaultClientSecret" `
     -kvc "$AzureKeyVaultCertificate" `
     -tr "$TimestampService" `
     -td sha384 `
