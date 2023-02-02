f5401e9fd270b53a63360359168e80e3a06844b2
Michael Riedmann <michael_riedmann@live.com>
2023-02-02 17:14:34 +0100

    fix: local build on win

--- 01-generate-manifests.ps1
+++ 01-generate-manifests.ps1
@@ -4,7 +4,7 @@ $tmpPath = Join-Path -Path (Resolve-Path .tmp).Path -ChildPath .\manifests
 New-Item -Path $tmpPath -Force -ItemType Directory
 
 Get-ChildItem .\packages -Filter '*.jsonnet' | % {
-    jsonnet -m $tmpPath -c ".\packages\$($_.Name)"
+    jsonnet -J . -m $tmpPath -c ".\packages\$($_.Name)"
 }
 
 Get-ChildItem .tmp -Recurse -Include '*.json' | % {
